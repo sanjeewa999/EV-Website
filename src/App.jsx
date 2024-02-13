933ab9472ebaeb1e965632e1a84dde36ae44d094
Sanjeewa Chathuranga <sanjeewachaturanga0@gmail.com>
2024-02-13 12:44:43 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+export const App = () => {
+
+  let heroData = [
+    { text1: "Dive into", text2: "what you love" },
+    { text1: "Indulge", text2: "your passions" },
+    { text1: "Give in to", text2: "your passions" },
+  ]
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false);
+
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount} />
+    </div>
+  )
+}
---
+++ src/Components/Background/Background.jsx
@@ -0,0 +1,7 @@
+import './Background.css'
+
+export const Background = (playStatus, heroCount) => {
+  return (
+    <div>Background</div>
+  )
+}
